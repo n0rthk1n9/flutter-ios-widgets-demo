b6121a688e01a3cb18130f057edea1e78ea8f6a9
Jan Armbrust <jan.armbrust@googlemail.com>
2023-10-21 13:47:40 +0200

    add weather data

---
+++ lib/weather_data.dart
@@ -0,0 +1,11 @@
+class Weather {
+  final String symbol;
+  final String condition;
+  final String temperature;
+
+  Weather({
+    required this.symbol,
+    required this.condition,
+    required this.temperature,
+  });
+}
